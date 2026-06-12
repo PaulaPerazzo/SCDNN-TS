ba0129e4c63ada2b3b0a081c455ea9303aad3ffc
paulaperazzo <mpps@cin.ufpe.br>
2026-06-12 12:11:41 -0300

    lf and hr ratios

--- script.sh
+++ script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=scdnn
 #SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
 #SBATCH --mem 32G
 #SBATCH -c 16
 #SBATCH -p short-simple
 #SBATCH -o job.log
-#SBATCH --output=job_output_2.txt
-#SBATCH --error=job_error_2.txt
+#SBATCH --output=job_output_3.txt
+#SBATCH --error=job_error_3.txt
 
 source env/bin/activate
 
 # python3 generate_data.py
-python3 train_temp.py --epochs 100 --task_num 1
+python3 train_temp.py --epochs 100 --task_num 2
