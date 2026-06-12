c3ad9e633d071c3dd35f5b93644745d1cb352a49
paulaperazzo <mpps@cin.ufpe.br>
2026-06-12 16:34:08 -0300

    model tested

--- script_dil.sh
+++ script_dil.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=scdnn_dil
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --mem 32G
+#SBATCH --mem 16G
 #SBATCH -c 16
 #SBATCH -p short-simple
 #SBATCH -o job.log
-#SBATCH --output=job_output_test_cnn_dilatada.txt
-#SBATCH --error=job_error_test_cnn_dilatada.txt
+#SBATCH --output=job_output_test_cnn_dilatada_opt1.txt
+#SBATCH --error=job_error_test_cnn_dilatada_opt1.txt
 
 source env/bin/activate
 
 # python3 generate_data.py
-python3 train_temp_dil.py --epochs 100 --task_num 2
+python3 train_temp_dil.py --epochs 100 --task_num 3
